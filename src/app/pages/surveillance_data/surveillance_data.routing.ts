1549b0a80b58669a2d3225200d800b7fc9a57519
otienoelvis <sevileelvis@gmail.com>
2024-04-16 06:23:27 +0300

    Notifications Bell Menu

--- src/app/pages/surveillance_data/surveillance_data.routing.ts
+++ src/app/pages/surveillance_data/surveillance_data.routing.ts
@@ -6,16 +6,13 @@ import {AuthGuard} from "../../services/authentication.service";
 export const SurveillanceDataRoutes: Routes = [
   {
     path: '',
-    // canActivate: [AuthGuard], data: { roles: [1, 2, 3] },
     children: [
       {
         path: '',
-        // canActivate: [AuthGuard], data: { roles: [1, 2, 3] },
         component: CompositeComponent,
       },
       {
         path: 'composites',
-        // canActivate: [AuthGuard], data: { roles: [1, 2, 3] },
         component: CompositeComponent,
       }, {
         path: 'modify',
